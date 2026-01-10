4bacd22e57243241ff4e84778768fdc083f8d7c0
David Flek <davidflek@backtech.cz>
2026-01-10 15:31:29 +0100

    feat: i18n installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@pinia/nuxt']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n'
+  ],
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US' },
+      // { code: 'fr', language: 'fr-FR' }
+    ],
+    defaultLocale: 'en',
+  }
 })
